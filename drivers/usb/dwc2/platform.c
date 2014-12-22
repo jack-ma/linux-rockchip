3d1d924277d31d408f2ffd37d89564299a8844f8
Romain Perier <romain.perier@gmail.com>
2014-12-22 19:42:54 +0000

    usb: dwc2: Register interrupt handler only once gadget is correctly initialized
    Don't register interrupt handler becore usb gadget is correctly initialized.
    For embedded platforms which don't have a usb-phy it will crash the driver
    because an interrupt might be emitted with non-initiliazed hardware.
    According to devm_request_irq documentation, an interrupt can be emitted
    at any time once the interrupt is registered, so we have to care about driver
    and hardware initialization.
    
    Signed-off-by: Romain Perier <romain.perier@gmail.com>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -196,14 +196,6 @@ static int dwc2_driver_probe(struct platform_device *dev)
 		return irq;
 	}
 
-	dev_dbg(hsotg->dev, "registering common handler for irq%d\n",
-		irq);
-	retval = devm_request_irq(hsotg->dev, irq,
-				  dwc2_handle_common_intr, IRQF_SHARED,
-				  dev_name(hsotg->dev), hsotg);
-	if (retval)
-		return retval;
-
 	res = platform_get_resource(dev, IORESOURCE_MEM, 0);
 	hsotg->regs = devm_ioremap_resource(&dev->dev, res);
 	if (IS_ERR(hsotg->regs))
@@ -237,6 +229,15 @@ static int dwc2_driver_probe(struct platform_device *dev)
 	retval = dwc2_gadget_init(hsotg, irq);
 	if (retval)
 		return retval;
+
+        dev_dbg(hsotg->dev, "registering common handler for irq%d\n",
+                irq);
+        retval = devm_request_irq(hsotg->dev, irq,
+                                dwc2_handle_common_intr, IRQF_SHARED,
+                                dev_name(hsotg->dev), hsotg);
+        if (retval)
+                return retval;
+
 	retval = dwc2_hcd_init(hsotg, irq, params);
 	if (retval)
 		return retval;
